be19db8e6afd64dc0a7e949f04316366124b7ca0
Autumn Raine <autumn.raine@icloud.com>
2018-04-12 10:26:28 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  username = '';
+
+  checkUsername() {
+    if(this.username === '') {
+      return 'Username cannot be blank';
+    } else {
+      return this.username;
+    }
+  }
 }
